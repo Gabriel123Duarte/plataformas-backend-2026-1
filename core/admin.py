644ad095064def06d9ab86f5997248fd15b3b800
Gabriel Duarte <gabriellagoa10@yahoo.com.br>
2026-04-15 19:54:15 -0300

    Aula

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from core.models import Aluno, Curso
+
+
 # Register your models here.
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "idade", "curso")
+
+
+admin.site.register(Aluno, AlunoAdmin)
+
+admin.site.register(Curso)
